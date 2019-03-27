5e31bebbb60f4cbbe9099904f83c23dac7b3924a
Nico <nicolaspalacio91@gmail.com>
2019-03-27 09:23:37 +0100

    Added some comments on psite_overlap script

--- validation/A_psite_overlap.py
+++ validation/A_psite_overlap.py
@@ -17,11 +17,11 @@ except:
     pass
 
 #=============================== LOADING DATA ================================#
-raw = pd.read_csv('data/raw_all.csv', index_col=0)
-rawb = raw[[c for c in raw.columns if 'B' in c]]
+raw = pd.read_csv('data/raw_all.csv', index_col=0) # Raw data all batches
+rawb = raw[[c for c in raw.columns if 'B' in c]] # Only untreated samples
 #raw = raw[sorted(raw.columns)]
 
-# p-sites with NaN in all samples
+# Check if p-sites with NaN in all samples
 (pd.isna(raw).sum(axis=1) == len(raw)).sum()
 
 #============================ PROPORTION OF NaNs =============================#
@@ -45,20 +45,25 @@ fig.tight_layout()
 fig.savefig('results/nan_vals.pdf')
 
 #=================== SAMPLE SIMILARITY - MEASURED P-SITES ====================#
+# For each sample, take the indices of the cells whose value is not NaN
 psites = dict([(c, set(rawb.index[pd.notna(rawb[c])])) for c in rawb.columns])
 
+# Initialize a square matrix for samples X samples
 sims = pd.DataFrame(index=rawb.columns,
                     columns=rawb.columns)
 
+# Magic numbers for label positions because not all batches have same size
 pos = [2.5, 7.5, 12.5, 17.5, 22.5, 27.5, 32.5, 37.5, 42.5, 46.5, 50.5]
 batches = ['Set1', 'Set2', 'Set3', 'Set4', 'Set5', 'Set6', 'Set7', 'Set8',
            'V1', 'V2', 'V3']
 
 # By similarity index
 for (name, mode) in [('Jaccard', 'j'), ('Sorensen-Dice', 'sd'), ('Szymkiewicz-Simpson', 'ss')]:
+    # Iteration over all possible pairs of samples
     for (a, b) in itt.product(rawb.columns, repeat=2):
         sims.loc[a, b] = similarity(psites[a], psites[b], mode=mode)
 
+    # Plotting heatmap for a given similarity index
     fig, ax = plt.subplots(figsize=(15, 15))
     im = ax.imshow(sims.values.astype(float), cmap='nipy_spectral', interpolation='none')
     fig.colorbar(im, shrink=0.75)
@@ -86,11 +91,12 @@ for (name, mode) in [('Jaccard', 'j'), ('Sorensen-Dice', 'sd'), ('Szymkiewicz-Si
     fig.savefig('results/%s_sim_psites_hist.pdf' % mode)
 
 #==================== P-SITE OVERLAP VALIDATION vs. TRAIN ====================#
-# Measured p-sites overlap (all training vs. vlaidation batches)
+# Measured p-sites overlap (all training vs. validation batches)
 v3 = rawb.columns[-5:].tolist()
 v2 = rawb.columns[-8:-5].tolist()
 v1 = rawb.columns[-13:-8].tolist()
 
+# Subsetting measured p-sites (IDs) in Train set and the validation batches
 sets = {'V1': set(), 'V2': set(), 'V3': set(), 'Train': set()}
 
 for (k, v) in psites.items():
@@ -111,19 +117,29 @@ plot = venn(sets.values(), labels=sets.keys(), sizes=True,
             filename='results/psite_overlap_train.pdf')
 
 #============== P-SITE OVERLAP MODELS vs. VALIDATION vs. TRAIN ===============#
+# Load list of predictors from the ensemble of models
 sdir = '../44_AML_ex_vivo/extreme_Rs_NRs/lasso_results'
 predictors = pd.read_csv(os.path.join(sdir, 'predictors.csv'), index_col=0)
 #predictors[pd.isna(predictors)] = 0
-predictors.shape
+#predictors.shape
 
-# Subsetting the predictors to the ones that had value > 0 in at least one model
+# Subsetting predictors to the ones that had value > 0 in at least one model
 predictors = predictors[pd.isna(predictors).sum(axis=1) < 100]
 predictors.shape
 
+# Adding for comparison to other subsets
 sets['Models'] = set(predictors.index)
 
 plot = venn(sets.values(), labels=sets.keys(), sizes=True, filename='results/psite_overlap_train_model.pdf')
 
+#=================== P-SITE OVERLAP MODELS vs. VALIDATION ====================#
+del sets['Train']
+v = [sets.values()[3]]+sets.values()[:3]
+k = [sets.keys()[3]]+sets.keys()[:3]
+plot = venn(v, labels=k, sizes=True,
+filename='results/psite_overlap_model.pdf')
+
+#================= P-SITE OVERLAP TRAIN (OLD) vs TRAIN (NEW) =================#
 # Double-check the p-site overlap of old dataset (train) vs new dataset (train
 # + validation)
 tdata = pd.read_csv('../44_AML_ex_vivo/data/raw.csv', index_col=0)
@@ -138,19 +154,13 @@ tdata = pd.read_csv('../44_AML_ex_vivo/data/raw.csv', index_col=0)
 plot = venn([set(raw.index), set(tdata.index)], labels=['NEW', 'OLD'], sizes=True,
             filename='results/oldVSnew.pdf')
 
-#=================== P-SITE OVERLAP MODELS vs. VALIDATION ====================#
-
-del sets['Train']
-v = [sets.values()[3]]+sets.values()[:3]
-k = [sets.keys()[3]]+sets.keys()[:3]
-plot = venn(v, labels=k, sizes=True,
-            filename='results/psite_overlap_model.pdf')
-
 #===================== CHECKING OLD vs NEW CORRELATION =======================#
+# List of overlapping p-sites between data files
 common = list(set(raw.columns).intersection(set(tdata.columns)))
 
 cors = dict()
 
+# Computing correlation for each p-site in both files
 for c in common:
     aux = raw[c].to_frame().merge(tdata[c].to_frame(), how='outer',
                                   right_index=True, left_index=True,
@@ -158,6 +168,7 @@ for c in common:
     cors[c] = aux.corr().iloc[0, 1]
     aux[pd.isna(aux)] = 0
 
+# Plot
 fig, ax = plt.subplots(figsize=(12, 5))
 rng = range(len(cors))
 ax.bar(rng, cors.values())
@@ -172,22 +183,26 @@ fig.tight_layout()
 fig
 fig.savefig('results/corr_samples.pdf')
 
+#====================+= CHECKING OLD vs NEW DIFFERENCE =======================#
 old = tdata.copy()
-old[pd.isna(old)] = 0
+old[pd.isna(old)] = 0 # Set NaN to 0
 new = raw.copy()
-new[pd.isna(new)] = 0
+new[pd.isna(new)] = 0 # Set NaN to 0
 
+# Compute the difference of raw intensities for common p-sites
 difs = pd.DataFrame([new[c] - old[c] for c in common], index=common).T
 difs[pd.isna(difs)] = 0
 
-difs.shape
+#difs.shape
 
+# Substet p-sites to those which have some difference in at least a sample
 difs = difs.loc[(difs != 0).sum(axis=1) > 1, :]
-difs.shape
-len(difs.columns)
-difs = difs[tdata.columns]
-len(difs.columns)
+#difs.shape
+#len(difs.columns)
+difs = difs[tdata.columns] # Reordering for batches
+#len(difs.columns)
 
+# Plot heatmap
 fig, ax = plt.subplots(figsize=(25, 12))
 
 im = ax.imshow(np.log(difs.values).T, aspect='auto',
