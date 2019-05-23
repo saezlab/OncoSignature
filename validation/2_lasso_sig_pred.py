21c2a40d6857efe437a348cf2afe69feb51a800e
Nico <nicolaspalacio91@gmail.com>
2019-05-23 15:21:17 +0200

    Updated path

--- validation/2_lasso_sig_pred.py
+++ validation/2_lasso_sig_pred.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 
-#os.chdir('/home/nico/Desktop/OncoSignature/ORGANIZED/validation')
+os.chdir('validation/')
 
 # Loading validation data
 data = pd.read_csv('data/norm_data.csv', index_col=0)
