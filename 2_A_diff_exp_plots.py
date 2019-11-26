c1c08dd1014d791b0098ba13dd4ff43a2b29d2ed
Nico <nicolaspalacio91@gmail.com>
2019-11-26 10:17:31 +0100

    Added DEX plotting script

---
+++ 2_A_diff_exp_plots.py
@@ -0,0 +1,61 @@
+# Copyright (C) 2019 Nicolàs Palacio
+#
+# Contact: nicolas.palacio@bioquant.uni-heidelberg.de
+#
+# GNU-GLPv3:
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# A full copy of the GNU General Public License can be found on
+# http://www.gnu.org/licenses/.
+#
+# Differential expression plots
+# =============================
+#
+# This script generates the volcano plots to visualize the differential
+# expression results
+
+import os
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from data_tools.plots import volcano
+
+#----------------------------------- INPUT -----------------------------------#
+dir_ = 'results/2_diff_exp'
+#-----------------------------------------------------------------------------#
+
+
+# Loading the results from the differential expression analysis
+files = [f for f in os.listdir(dir_) if f.endswith('_ttop.csv')]
+
+for f in files:
+    name = ' vs. '.join(f.split('_')[0:2])
+
+    df = pd.read_csv(os.path.join(dir_, f), index_col=0)
+
+    # Volcano plot
+    volcano(df['logFC'], -np.log10(df['P.Value']), title=name,
+            filename=os.path.join(dir_, '%s_%s.pdf' %tuple(f.split('_')[0:2])))
+
+    # p-value histogram
+    fig, ax = plt.subplots()
+
+    ax.hist(df['P.Value'].dropna(), bins=100)
+    ax.set_xlim(0, 1)
+    ax.set_xlabel('P.Value')
+    ax.set_ylabel('Frequency')
+    ax.set_title('%s : p-value histogram' %name)
+
+    fig.tight_layout()
+
+    fig.savefig(os.path.join(dir_,
+                             '%s_%s_pval_hist.pdf' %tuple(f.split('_')[0:2])))
